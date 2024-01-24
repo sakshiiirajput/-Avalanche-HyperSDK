fb8b59be081ef1302802d69c16611039cb1e2e1f
sakshiiirajput <150885602+sakshiiirajput@users.noreply.github.com>
2024-01-25 00:58:36 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "bucsit"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "bucsitcoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "BCN"
 )
 
 var ID ids.ID
